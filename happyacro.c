bde04d9beb65afe78ae4a3593b6c126c411e9e73
Happy Acro <teamplayer@happyacro.com>
2018-10-19 10:52:14 -0700

    Typo.

--- happyacro.c
+++ happyacro.c
@@ -9,7 +9,7 @@ Documentation for this program is available here:
 
 https://github.com/happyacro/happyacro-dos
 
-Copyright [c) 2017, Coder Cowboy, LLC. All rights reserved.
+Copyright (c) 2017, Coder Cowboy, LLC. All rights reserved.
 
 Redistribution and use in source and binary forms, with or without
 modification, are permitted provided that the following conditions are met:
